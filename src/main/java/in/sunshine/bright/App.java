8de395b3f1fb4137db4efd483c2fef41ea5eafcb
pb4gcp <45336876+pb4gcp@users.noreply.github.com>
2019-01-31 13:20:15 +0530

    More exclaimations.

--- src/main/java/in/sunshine/bright/App.java
+++ src/main/java/in/sunshine/bright/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World.. Good Afternoon!" );
+        System.out.println( "Hello World.. Good Afternoon!!!" );
     }
 }
